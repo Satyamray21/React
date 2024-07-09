115d4603f9ad0b9d0a0545a4b5a2e40804dcedfb
Satyamray21 <satyamray43@gmail.com>
2024-07-09 11:00:20 +0530

    1st project using react

---
+++ React1project/1project/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Firs from './first.jsx';
+function App() {
+  return (
+    <>
+     <h1>Hello my first program in react</h1>
+    <Firs />
+    </>
+   
+  );
+}
+
+export default App;
---
+++ React1project/1project/src/first.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function First(){
+    return(
+<h1>Uing external file</h1>
+    );
+}
+export default First;
